d6ce5457cca3910455b53b6aaf6156c30afa50b7
Anna Markovskaya <a.s.markovskaya@gmail.com>
2023-12-14 11:14:07 +0300

    Добавила класс для отдельных игрушек, методы в основную часть

--- singleToy.java
+++ singleToy.java
@@ -1,14 +1,13 @@
 package toyshop_draw;
 
-public class toy {
+public class singleToy {
     private int toyID;
     private String name;
-    private int qty;
     private int drawWeight;
 
-    public Toy(String name, int quantity, int weight){
+    public singleToy(String name, int ID, int weight){
         this.name = name;
-        this.qty = quantity;
+        this.toyID = ID;
         this.drawWeight = weight;
     }
 
@@ -19,41 +18,18 @@ public class toy {
     public Integer getId(){
         return toyID;
     }
-    
-    public Integer getQuantity(){
-        return qty;
-    }
-    
+            
     public Integer getWeight(){
         return drawWeight;
     }
-
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public void setQuantity(Integer quantity){
-        this.qty = quantity;
-    }
-
-    public void setWeight(Integer weight){
-        this.drawWeight = weight;
-    }
-
-    public void addQuantity(Integer quantity){
-        this.qty += quantity;
-    }
-
+    
     @Override
     public String toString(){
         return "Toy{" + 
                 "ID=" + String.valueOf(toyID) + 
                 "; name=" + name + 
-                "; quantity=" + String.valueOf(qty) +
                 "weight=" + String.valueOf(drawWeight) + '\'' +
                 '}';
 
     }
-
-
 }
--- toyshop_draw.java
+++ toyshop_draw.java
@@ -1,9 +1,38 @@
 package toyshop_draw;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /**
  * toyshop_draw
  */
 public class toyshop_draw {
+
+    public static void createToy(){
+        // создаем доп. игрушку (toy), запрашиваем все поля, 
+        // проверяем несовпадение названия, присваиваем ИД
+    }
+
+    public static void changeToy(){
+        // изменяем имеющуюся игрушку (toy), 
+        // запрашиваем, что изменить (вес, название, кол-во НЕ ИД) 
+    
+    }
+
+    public static PriorityQueue addToyToQueue(PriorityQueue tmpQueue, singleToy Toy){
+        // проверяем, есть ли еще игрушки такого типа (ID есть, qty > 0)
+        // добавлем в очередь
+        // уменьшаем кол-во в классе Toy
+        // возвраащем обновленную очередь
+    }
+
+    public static void drawTime(PriorityQueue tmpQueue){
+        // берем самую старую в очереди
+        // генерируем случайное число 1-100
+        // если <= весу, выписываем "Вы выигради" и поля игрушки в консоль и файл, игрушка удаляется из очереди
+        // если больше веса, то выписываем в консоль "увы" и ничего не меняем
+
+    }
 
     public static void main(String[] args) {
         
